555a5d555ff54ed3fcea40b40325480d4b8681ab
alanAval <alan@avaltech.com.br>
2019-10-14 12:32:51 -0300

    Create Work Flow for App Messages

--- src/gateway_src/gateway_server.java
+++ src/gateway_src/gateway_server.java
@@ -1,11 +1,15 @@
 package gateway_src;
 import java.net.*;
 import java.util.ArrayList;
+
+import com.google.protobuf.UninitializedMessageException;
+
 import java.io.*;
 import java.time.*;
 //import protoClass.SensorOuterClass;
 import protoClass.SensorOuterClass.CommandMessage;
 import protoClass.SensorOuterClass.Sensor;
+import protoClass.SensorOuterClass.Sensor.SensorType;
 //import protoClass.SensorOuterClass.Sensor.SensorType;
 import protoClass.SensorOuterClass.CommandMessage.CommandType;
 
@@ -15,7 +19,7 @@ public class gateway_server {
 		
 		ArrayList<sensorBuff> sensorList = new ArrayList<sensorBuff>();
 		
-		int serverPort = 7777; // the server port
+		int serverPort = 6543; // the server port
 		int sensorPort = 8888; // the sensor port
 		try {
 			//Código_de_mensagem_de_descoberta
@@ -32,6 +36,7 @@ public class gateway_server {
 			
 			listenSocket = new ServerSocket(serverPort);
 			while (true) {
+				System.out.println("Ouvindo...");
 				Socket clientSocket = listenSocket.accept();
 				ConnectionTCP c = new ConnectionTCP(clientSocket, sensorList);
 				c.start();
@@ -52,6 +57,7 @@ class ConnectionTCP extends Thread {
 
 	public ConnectionTCP(Socket aClientSocket, ArrayList<sensorBuff> sensorList) throws IOException, EOFException {
 		try {
+			System.out.println("Received attempt to connect!");
 			clientSocket = aClientSocket;
 			this.sensorList = sensorList;
 			in = new DataInputStream(clientSocket.getInputStream());
@@ -61,28 +67,81 @@ class ConnectionTCP extends Thread {
 			
 		} catch (IOException e) {
 			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
+	
+	public Sensor getSensorById(int id, SensorType type) {
+		if (id == -1) {
+			for (int i = 0; i < sensorList.size() -1; i++) {
+				if (sensorList.get(i).getSensor().getType() == type) {
+					return sensorList.get(i).getSensor();
+				}
+			}
+		}
+		for (int i = 0; i < sensorList.size() -1; i++) {
+			if (sensorList.get(i).getSensor().getId() == id) {
+				return sensorList.get(i).getSensor();
+			}
+		}
+		return null;
+	}
+	
+	public void removeSensorById(int id) {
+		for (int i = 0; i < sensorList.size() -1; i++) {
+			if (sensorList.get(i).getSensor().getId() == id) {
+				sensorList.remove(sensorList.get(i));
+				break;
+			}
+		}
+	}
+	
+	public void updateSensorById(int id, Sensor sensor) {
+		for (int i = 0; i < sensorList.size() -1; i++) {
+			if (sensorList.get(i).getSensor().getId() == id) {
+				sensorList.get(i).setSensor(sensor);
+			}
+		}
+	}
+	
+	public void handleMessage(CommandMessage msg) throws IOException{
+		if (msg.getCommand() == CommandType.GET_STATE) {
+			cmd.setParameter(getSensorById(msg.getParameter().getId(), msg.getParameter().getType()));
+			byte[] sendData = cmd.build().toByteArray();
+			out.write(sendData);
+		}
+		else if (msg.getCommand() == CommandType.SET_STATE) {
+			Sensor sensor = getSensorById(msg.getParameter().getId(), msg.getParameter().getType());
+			Sensor.Builder sensorBuilder = sensor.toBuilder();
+			sensorBuilder.setState(msg.getParameter().getState());
+			updateSensorById(sensor.getId(), sensorBuilder.build());
+			cmd.setParameter(sensorBuilder.build());
+			byte[] sendData = cmd.build().toByteArray();
+			out.write(sendData);
+		}
+		// não entendi o que é o SENSOR_STATE, mas se tiver que ser visto aqui, adiciona
 	}
 
 	public void run() {
 		try {
-			cmd.build().getParserForType().parseFrom(in);
-			byte[] sendData = null;
-			
-			if(cmd.hasCommand() && cmd.hasParameter()){
-				if(sb.containSensorPerID(cmd.getParameter(), sensorList)) {
-					cmd.setParameter(sensorList.get(sb.sensorListIndex(cmd.getParameter(), sensorList)).getSensor());
-					cmd.setCommand(null);
-					sendData = cmd.build().toByteArray();
-					out.write(sendData);
-				}
+			//repete o processo até dar um erro, ai fecha o socket.
+			while (true) {
+				byte[] request = new byte[1024];
+				System.out.println("Preparing to read...");
+				in.read(request);
+				System.out.println("Message read");
+				CommandMessage cmdMessage = CommandMessage.parseFrom(request);
+				handleMessage(cmdMessage);
 			}
 			
 		} catch (EOFException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
+		} catch (UninitializedMessageException e) {
+		}
+		finally {
 				try {
 					clientSocket.close();
 					in.close();
